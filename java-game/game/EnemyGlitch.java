28efd6fd78240ba5ceb0f1983b164dcaec6aed84
lbehr19 <50720409+lbehr19@users.noreply.github.com>
2019-07-31 15:01:41 -0400

    Update movement function, constructor
    the class' update function is now fixed to resolve issue #10. Additionally, changes made to the constructor to allow for an easy way to change the number of enemies on the field, for instance when levels change.

--- java-game/game/EnemyGlitch.java
+++ java-game/game/EnemyGlitch.java
@@ -17,13 +17,16 @@ public class EnemyGlitch
     ArrayList<Sprite> glist = new ArrayList<Sprite>();
     
     private Image GLITCH_IMAGE = new Image("glitch_sprite.png", 50, 50, false, false);
-    private int ENEMY_COUNT = 4;
+    private int MAX_ENEMY_COUNT = 4;
+    private int MIN_ENEMY_COUNT = 1;
     private ArrayList<Pair<Integer, Integer>> ENEMY_POS = new ArrayList<Pair<Integer, Integer>>();
     
     
-    public EnemyGlitch(Walls w) {
+    public EnemyGlitch(Walls w, int spawnCount) {
+    	int enemyCount = spawnCount > 0 ? Math.min(spawnCount, MAX_ENEMY_COUNT) : MIN_ENEMY_COUNT; 
+    	//if supplied number is valid (pos, non-zero number), use it (if it's less than max). Else, use min.
         enemySpawns();
-        for (int i = 0; i < ENEMY_COUNT; i++) {
+        for (int i = 0; i < enemyCount; i++) {
             Sprite babyGlitch = new Sprite(w);
             babyGlitch.setImage(GLITCH_IMAGE);
             babyGlitch.setPos(ENEMY_POS.get(i));
@@ -37,28 +40,33 @@ public class EnemyGlitch
         Pair<Integer, Integer> playerPos = Player.getPos();
         int pX = playerPos.first();
         int pY = playerPos.second();
-        int dx = 0;
-        int dy = 0;
+        int dx;
+        int dy;
+        //boolean checkY = true;
         while(gliter.hasNext()) {
             Sprite glitch = gliter.next();
             Pair<Integer, Integer> gPos = glitch.getPos();
             int x = gPos.first();
             int y = gPos.second();
-            if ((pX - x) > 0) {
-            	dx = 1;
-            } else if ((pX - x) < 0) {
+            //start by deciding whether to move left or right
+            if (pX < x) {
             	dx = -1;
-            }
-            if (!glitch.update(dx, dy)) {
+            } else if (pX > x) {
+            	dx = 1;
+            } else {
             	dx = 0;
-            	if ((pY - y) > 0) {
-            		dy = 1;
-            	} else if ((pY - y) < 0){
+            }
+            //if left/right movement doesn't work, or glitch doesn't have to move left/right,
+            //move on to decide whether to move up/down.
+            if (!(dx != 0 && glitch.update(dx, 0))) {
+            	if (pY < y) {
             		dy = -1;
+            	} else if (pY > y) {
+            		dy = 1;
             	} else {
             		dy = 0;
             	}
-            	glitch.update(dx, dy);
+            	glitch.update(0, dy);
             }
         }
     }
