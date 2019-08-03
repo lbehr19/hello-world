4593f3f6d756965de80ba0241a3909efab99b20a
lbehr19 <50720409+lbehr19@users.noreply.github.com>
2019-08-03 15:49:09 -0400

    Update Sprite.java
    Part of the adding coins change. This one was mostly to make it so that the door's location was centralized in the Main file, so now every time a new Sprite is made, the door location has to be passed in.

--- java-game/game/Sprite.java
+++ java-game/game/Sprite.java
@@ -13,31 +13,27 @@ public class Sprite {
     private Image image;
     private int posX, posY;
     boolean hasKey;
-    private Pair<Integer, Integer> DOOR_POS = new Pair<Integer, Integer>(8, 1);
+    private Pair<Integer, Integer> DOOR_POS;
     
     Walls w;
-    //The Sprite class has to have an internal representation of the walls because 
-    //it manages a sprite's movement; it must ensure that sprites are not moving into walls. 
-    
-    public Sprite(Walls walls) {
+
+    public Sprite(Walls walls, Pair<Integer, Integer> door) {
         posX = 0;
         posY = 0;
         w = walls;
         hasKey = false;
+        DOOR_POS = door;
     }
 
-    //sets the sprite's location to a specific point; assumes the point is not on top of walls. 
     public void setPos(Pair<Integer, Integer> pair) {
         posX = pair.first();
         posY = pair.second();
     }
 
-    //sets the sprite's image
     public void setImage(Image i) {
         image = i;
     }
 
-    //Provided a direction/velocity, sets the sprite's new location (as long as it is not inside of a wall)
     public boolean update(int dx, int dy) {
         Pair<Integer, Integer> temPos = new Pair<Integer, Integer>(posX+dx, posY+dy);
         if (w.contains(temPos) || ((!hasKey)&&(temPos.equals(DOOR_POS)))) {
@@ -49,12 +45,10 @@ public class Sprite {
         }
     }
 
-    //redraws the image to a specific GraphicsContext
     public void render(GraphicsContext gc) {
         gc.drawImage(image, posX*50, posY*50);
     }
 
-    //returns the Sprite's current location
     public Pair<Integer, Integer> getPos() {
         return new Pair<Integer, Integer>(posX, posY);
     }
